dee16d75747b9097ec18e411c0547e2dfcf1d84e
SH4DY <marshall871018@gmail.com>
2022-10-10 11:20:35 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.set('view-engine')
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+app.get('/page2', (req, res) => {
+    res.render('page2.ejs')
+})
+app.get('/result', (req, res) => {
+    res.render('result_page.ejs')
+})
+
+app.listen(3000)
