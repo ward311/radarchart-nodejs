a18f91b67f39e1b2428c8fd658ac9aa66038670b
JackLab1029 <marshall871018@gmail.com>
2022-10-10 22:44:03 +0800

    change port number and css revision

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5500;
+const port = 3000;
 app.set('view-engine')
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
