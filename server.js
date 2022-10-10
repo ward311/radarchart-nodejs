b76cc2e98df620e0261ef2875b807871e63afa13
SH4DY <marshall871018@gmail.com>
2022-10-10 11:30:21 +0800

    revision commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 app.set('view-engine')
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
