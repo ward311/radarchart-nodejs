b9ebe93dcf067efc57e037e41a62789b17a87f67
SH4DY <marshall871018@gmail.com>
2022-10-16 04:39:06 +0800

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('view-engine')
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
